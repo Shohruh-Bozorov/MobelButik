8db8be323c114c27a4e10b7e743e1f8ba6f42e0f
Vidar Fröberg Tinell <vidar.tinell@gmail.com>
2022-02-15 11:07:15 +0100

    klart

--- MobelButik/Methods.cs
+++ MobelButik/Methods.cs
@@ -606,7 +606,6 @@ namespace MobelButik
                             select new { Produkt = korg.ProduktId }).ToList();
                 var connString = "Server=tcp:mobelbutik.database.windows.net,1433;Initial Catalog=Newton;Persist Security Info=False;User ID=vidrusen;Password=troll100!;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;";
                 var sql = $"select COUNT(id) as 'AntalKunder' from Kund";
-                var sql2 = $"select COUNT(id) as 'AntalID' from OrderHistorik";
 
                 using (var connection = new SqlConnection(connString))
                 {
@@ -614,30 +613,18 @@ namespace MobelButik
 
                     var num = connection.Query<int>(sql);
                     int kundId = num.AsList()[0];
-                    var num1 = connection.Query<int>(sql);
-                    int OHId = num.AsList()[0] + 1;
+
 
 
 
                     foreach (var prod in data)
                     {
-                        int orderHistorikID = Models.OrderHistorik.kundnr++;
-                        //var connString1 = "Server=tcp:mobelbutik.database.windows.net,1433;Initial Catalog=Newton;Persist Security Info=False;User ID=vidrusen;Password=troll100!;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;";
+                        var sql2 = $"select COUNT(id) as 'AntalID' from OrderHistorik";
+                        var num1 = connection.Query<int>(sql2);
+                        int OHId = num1.AsList()[0]+1;
                         var sql1 = $"insert into OrderHistorik values ({OHId}, {kundId}, {prod.Produkt})";
                         connection.Execute(sql1);
-                        /*using (var connection1 = new SqlConnection(connString))
-                        {
-                            connection.Open();
-                            try
-                            {
-                                connection.Execute(sql);
 
-                            }
-                            catch (Exception e)
-                            {
-                                Console.WriteLine(e.Message);
-                            }
-                        }*/
                     }
                 }
             }
@@ -650,7 +637,6 @@ namespace MobelButik
                 var result = from
                              orderHistorik in db.OrderHistoriks
                              select new { KundID = orderHistorik.KundId, ProduktId = orderHistorik.ProduktId };
-                //group Produkts by Produkts.ProduktNamn;
 
                 foreach (var product in result)
                 {
@@ -729,3 +715,23 @@ namespace MobelButik
     }
 }
 
+var connString = "Server=tcp:mobelbutik.database.windows.net,1433;Initial Catalog=Newton;Persist Security Info=False;User ID=vidrusen;Password=troll100!;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;";
+var sql = $"select COUNT(id) as 'Antal färger' from Färg";
+
+using (var connection = new SqlConnection(connString))
+{
+    connection.Open();
+    try
+    {
+        var num = connection.Query<int>(sql);
+        Console.WriteLine(num.AsList()[0]);
+
+    }
+    catch (Exception e)
+    {
+        Console.WriteLine(e.Message);
+    }
+
+
+
+}
--- MobelButik/Program.cs
+++ MobelButik/Program.cs
@@ -8,46 +8,7 @@ namespace MobelButik
     {
         static void Main(string[] args)
         {
-            var connString = "Server=tcp:mobelbutik.database.windows.net,1433;Initial Catalog=Newton;Persist Security Info=False;User ID=vidrusen;Password=troll100!;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;";
-            var sql = $"select COUNT(id) as 'Antal färger' from Färg";
 
-            using (var connection = new SqlConnection(connString))
-            {
-                connection.Open();
-                try
-                {
-                    var num = connection.Query<int>(sql);
-                    Console.WriteLine(num.AsList()[0]);
-
-                }
-                catch (Exception e)
-                {
-                    Console.WriteLine(e.Message);
-                }
-
-
-
-            }
-
-            /*using (var db = new MobelButik.Models.NewtonContext())
-            {
-
-
-                var data = (from f in db.Färgs
-                            
-                            select new
-                            {
-                                Antal = f.Count()
-
-                            }).ToList();
-
-
-                foreach (var prod in data)
-                {
-                    Console.WriteLine($"{prod.Id,-5} {prod.Namn,-10} {prod.Pris}");
-                }
-
-            }*/
             Console.WriteLine("Välkommen Till vår Webbshop!");
             Console.WriteLine("__________________________________________");
             bool quit = false;
